4bae5181be81ffb980045a55bba1ddca5ff3cace
Stas Frid <StasAleinikov@gmail.com>
2013-12-15 13:47:27 +0200

    discoAPI internal development options added

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,18 +9,33 @@ module.exports = function(grunt) {
 			},
 			build: {
 				src: 'src/<%= pkg.name %>.js',
-				dest: 'build/<%= pkg.name %>.min.js'
+				dest: 'build/<%= pkg.name %>.<%= pkg.version %>.min.js'
 			}
 		},
 		qunit: {
 			files: ['tests/index.html']
+		},
+		watch:{
+			uglify: {
+				files: 'src/<%= pkg.name %>.js',
+				tasks: ['uglify','discoAPICopy']
+			}
+		},
+		discoAPICopy: {
+			src: 'build/<%= pkg.name %>.<%= pkg.version %>.min.js',
+			files: ['<%= pkg.name %>.<%= pkg.version %>.min.js','<%= pkg.name %>.latest.min.js'],
+			dest: '../public_html/js/lib/'
 		}
+				
 	});
+
 
 	// Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	// qunit
 	grunt.loadNpmTasks('grunt-contrib-qunit');
+	// watch
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// Default task(s).
 	grunt.registerTask('default', ['uglify']);
@@ -28,4 +43,15 @@ module.exports = function(grunt) {
 	// other tests
 	grunt.registerTask('test', ['qunit']);
 
+	// discoAPI devel
+	grunt.registerTask('discoAPIDevel', ['watch']);
+
+	grunt.registerTask('discoAPICopy', 'copy file into discoAPI public_html', function() {
+		var config = grunt.config.get('discoAPICopy');
+
+		for(var i=0;i<config.files.length;i++){
+			grunt.file.copy(config.src,config.dest+config.files[i]);
+		}
+
+	});
 };
